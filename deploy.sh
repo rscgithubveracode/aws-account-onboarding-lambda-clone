0bf9b4fb1cde143a6e5b6054befb6f4bfd0eecf6
kavudhami <126605367+kavudhami@users.noreply.github.com>
2023-03-09 12:56:28 -0800

    chore: Added a script for deploying an applicatioin stack for the lambda function using the configuration in template.yml file. This will take care of creating the lambda function too.

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+set -eo pipefail
+ARTIFACT_BUCKET=$(cat bucket-name.txt)
+TEMPLATE=template.yml
+./gradlew build -i
+echo "Packaging local artifacts to generate a template file out.yml with s3 references instead of local paths..."
+aws --profile $1 --region $2 cloudformation package --template-file $TEMPLATE --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml # this will generate a template file out.yml where the local paths are replaced with the S3 URIs.
+echo "Generated out.yml file..."
+echo "Deploying artifacts as a cloudformation stack $3 for profile $1 in region $2"
+aws --profile $1 --region $2 cloudformation deploy --template-file out.yml --stack-name $3 --capabilities CAPABILITY_NAMED_IAM # this will deploy our lambda function artifact as an application using the template file
+echo "DONE"
